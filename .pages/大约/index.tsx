4383a40943b31c42009a96487482ce6fae9bf3a0
kkllooll <128013328+kkllooll@users.noreply.github.com>
2023-04-06 22:48:00 +0800

    Update and rename pages/about/index.tsx to .pages/大约/index.tsx

--- .pages/大约/index.tsx
+++ .pages/大约/index.tsx
@@ -1,15 +1,18 @@
-import React from "react"
+import React from "react";
 
 export default function About() {
   return (
     <div className="max-w-lg mx-auto mt-20 text-2xl">
       这是一个迷你博客网站，使用DummyJSON API。可供学习使用。源码：
       <a
-        href="https://github.com/liudi4046/twitter-clone"
+        href="https://github.com/kkllooll/-clone"
+        target="_blank"
+        rel="noopener noreferrer"
         className="hover:cursor-pointer underline hover:text-blue-400"
       >
-        https://github.com/liudi4046/twitter-clone
+        https://github.com/kkllooll/-clone
       </a>
     </div>
-  )
+  );
 }
+
